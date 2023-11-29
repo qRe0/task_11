d4ff000b86471449f2c3322b770072b8f9c5d8ab
qRe <melsenmalina@gmail.com>
2023-11-29 10:45:54 +0300

    Its WORKING!!!

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <algorithm>
+#include <functional>
+#include <fstream>
 
 using namespace std;
 
-FILE *in = freopen("in.txt", "r", stdin);
-FILE *out = freopen("out.txt", "w", stdout);
+ifstream in("in.txt");
+ofstream out("out.txt");
 
 struct TreeNode {
-    int value;
     int height;
     int subtreeSum;
     TreeNode *left;
     TreeNode *right;
+    int value;
 
-    TreeNode() : value(0), height(0), subtreeSum(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int key) : value(key), height(0), subtreeSum(0), left(nullptr), right(nullptr) {}
+    TreeNode(int key) : height(0), subtreeSum(0), left(nullptr), right(nullptr), value(key) {}
 };
 
 struct BST {
@@ -22,146 +23,187 @@ struct BST {
 
     BST() : root(nullptr) {}
 
-    void preOrderTraversal(TreeNode *node) {
-        if (node != nullptr) {
-            cout << node->value << endl;
-            preOrderTraversal(node->left);
-            preOrderTraversal(node->right);
-        }
-    }
-
     void insert(int key) {
         if (root == nullptr) {
             root = new TreeNode(key);
+            return;
         }
-        TreeNode *temp = root;
-        TreeNode *prev = nullptr;
-        int nodeComparison = 0;
-        while (temp != nullptr) {
-            prev = temp;
-            nodeComparison = key - temp->value;
-            if (nodeComparison > 0) {
-                temp = temp->right;
-            } else if (nodeComparison < 0) {
-                temp = temp->left;
-            } else if (nodeComparison == 0) {
+
+        TreeNode *tmp = root;
+        TreeNode *previous = nullptr;
+        while (tmp) {
+            previous = tmp;
+            if (key < tmp->value) {
+                tmp = tmp->left;
+            } else if (key > tmp->value) {
+                tmp = tmp->right;
+            } else {
+                // Key already exists in tree
                 return;
             }
         }
-        if (nodeComparison > 0) {
-            prev->right = new TreeNode(key);
-        } else if (nodeComparison < 0) {
-            prev->left = new TreeNode(key);
-        }
-    }
 
-    void heightAndSubtreeSum(TreeNode *node) {
-        if (node->right != nullptr) {
-            heightAndSubtreeSum(node->right);
-            node->height = node->right->height;
+        if (key < previous->value) {
+            previous->left = new TreeNode(key);
         } else {
-            node->height = 0;
-        }
-        if (node->left != nullptr) {
-            heightAndSubtreeSum(node->left);
-            node->subtreeSum = node->left->height;
-        } else {
-            node->subtreeSum = 0;
-        }
-        if (node->right != nullptr) {
-            node->subtreeSum += node->right->height;
-            node->height = max(node->height, node->right->height + 1);
-        }
-        node->subtreeSum += node->height;
-    }
-
-    void findRootOfMaxSubtreeSumPath() {
-        int maximum = 0;
-        TreeNode *maxRoot = nullptr;
-        findRootOfMaxSubtreeSumPath(root, maximum, maxRoot);
-        if (maxRoot != nullptr) {
-            deleteRoot_right(maxRoot, nullptr, root);
+            previous->right = new TreeNode(key);
         }
     }
 
-    void findRootOfMaxSubtreeSumPath(TreeNode *node, int &maximum, TreeNode *&maxRoot) {
-        if (node->left != nullptr) {
-            findRootOfMaxSubtreeSumPath(node->left, maximum, maxRoot);
-        }
-        if (node->subtreeSum > maximum) {
-            maximum = node->subtreeSum;
-            maxRoot = node;
-        }
-        if (node->right != nullptr) {
-            findRootOfMaxSubtreeSumPath(node->right, maximum, maxRoot);
+    int maximum;
+
+    void subtreeHeight(TreeNode *root) {
+        int leftH;
+        int rightH;
+
+        if (root->right) {
+            subtreeHeight(root->right);
+            rightH = root->right->height;
+        } else rightH = 0;
+
+        if (root->left != nullptr) {
+            subtreeHeight(root->left);
+            leftH = root->left->height;
+        } else leftH = 0;
+
+        root->height = (std::max(rightH, leftH) + 1);
+        root->subtreeSum = rightH + leftH;
+        if (root->subtreeSum > maximum) {
+            maximum = root->subtreeSum;
         }
     }
 
-    void deleteRoot_right(TreeNode *node, TreeNode *prev, TreeNode *&root) {
-        if (node == root) {
-            if (node->left == nullptr) {
+    bool flag = false;
+
+    void searchRoot(TreeNode *root, TreeNode *previous) {
+        if (root->left != nullptr) {
+            searchRoot(root->left, root);
+        }
+
+        if (root->subtreeSum == maximum) {
+            if (!flag) {
+                TreeNode *nodeRoot;
+                nodeRoot = root;
+                flag = true;
+                cout << nodeRoot->value << " is Root" << endl;
+                deleteRoot_right(nodeRoot, previous);
+            }
+        }
+        if (root->right != nullptr) {
+            searchRoot(root->right, root);
+        }
+    }
+
+    void deleteRoot_right(TreeNode *root, TreeNode *previous) {
+        if (root)
+        {
+            if (root->left == nullptr)
+            {
                 root = root->right;
-            } else if (node->right == nullptr) {
+            }
+            if (root->right == nullptr)
+            {
                 root = root->left;
             }
+
         }
-        if (node->left == nullptr && prev != nullptr) {
-            if (prev->left == node) {
-                prev->left = node->right;
-            } else if (prev->right == node) {
-                prev->right = node->right;
+
+        if (root->left == nullptr && previous != nullptr)
+        {
+            if (previous->left == root)
+            {
+                previous->left = root->right;
+            }
+            if (previous->right == root)
+            {
+                previous->right = root->right;
             }
         }
-        if (node->right == nullptr && prev != nullptr) {
-            if (prev->left == node) {
-                prev->left = node->left;
-            } else if (prev->right == node) {
-                prev->right = node->left;
+        if (root->right == nullptr && previous != nullptr)
+        {
+            if (previous->left == root)
+            {
+                previous->left = root->left;
+            }
+            if (previous->right == root)
+            {
+                previous->right = root->left;
             }
         }
-        if (node->left != nullptr && node->right != nullptr) {
-            TreeNode *tempNode = node->right;
-            TreeNode *tempPrev = node;
-            while (true) {
-                if (tempNode->left != nullptr) {
-                    tempPrev = tempNode;
+
+
+        if ((root->right != nullptr) && (root->left != nullptr))
+        {
+            TreeNode *tempNode = root->right;
+            TreeNode *tempPred = root;
+            while (true)
+            {
+                if (tempNode->left != nullptr)
+                {
+                    tempPred = tempNode;
                     tempNode = tempNode->left;
-                } else {
-                    break;
+                }
+                else break;
+            }
+            root->value = tempNode->value;
+            if (tempNode->right != nullptr)
+            {
+                if (tempPred != root)
+                {
+                    tempPred->left = tempNode->right;
+                }
+                else
+                {
+                    tempPred->right = tempNode->right;
                 }
             }
-            node->value = tempNode->value;
-            if (tempNode->right != nullptr) {
-                if (tempPrev != node) {
-                    tempPrev->left = tempNode->right;
-                } else {
-                    tempPrev->right = tempNode->right;
+            else
+            {
+                if (tempPred != root)
+                {
+                    tempPred->left = nullptr;
                 }
-            } else {
-                if (tempPrev != node) {
-                    tempPrev->left = nullptr;
-                } else {
-                    tempPrev->right = nullptr;
+                else
+                {
+                    tempPred->right = nullptr;
                 }
             }
         }
     }
+
+    void preOrderTravesal(TreeNode *root) {
+        if (root != nullptr) {
+            out << root->value << "\n";
+            preOrderTravesal(root->left);
+            preOrderTravesal(root->right);
+        }
+    }
+
 };
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     BST tree;
-
     int key;
-    while (cin >> key) {
+    while (in >> key) {
         tree.insert(key);
     }
 
-    tree.heightAndSubtreeSum(tree.root);
-    tree.findRootOfMaxSubtreeSumPath();
-    tree.preOrderTraversal(tree.root);
+    cout << "rootHeights of each root:" << endl;
+    tree.subtreeHeight(tree.root);
+    function<void(TreeNode *)> printHeights = [&](TreeNode *root) {
+        if (root == nullptr) {
+            return;
+        }
+        cout << "TreeNode " << root->value << " has height: " << root->height << endl;
+        printHeights(root->left);
+        printHeights(root->right);
+    };
+
+    printHeights(tree.root);
+
+    cout << "\nDeleting root of subtree with maximum sum:" << endl;
+    tree.searchRoot(tree.root, nullptr); // Поиск и удаление корня полупути
+    tree.preOrderTravesal(tree.root);
 
     return 0;
 }
